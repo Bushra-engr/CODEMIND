105699b572fd2cf21237384c4a5f463bb7e17816
Bushra khan <Bushra khan>
2026-07-05 15:26:50 +0530

    changed connection file

--- backend/databases/connection.py
+++ backend/databases/connection.py
@@ -7,7 +7,15 @@ load_dotenv()
 
 Database_url = os.getenv("DATABASE_URL")
 
-engine = create_engine(Database_url)
+engine = create_engine(
+    Database_url,
+    pool_pre_ping=True,
+    pool_recycle=180,
+    pool_timeout=30,
+    pool_size=5,
+    max_overflow=10,
+    echo=True,
+)
 
 SessionLocal = sessionmaker(
     bind = engine,
